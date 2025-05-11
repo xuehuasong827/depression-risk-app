1a8cb953a24384b49b4b48f4c4c32d56a268cc3a
xuehuasong827 <xuehuasong827@gmail.com>
2025-05-11 16:54:12 +0800

    Fix FileExistsError on images directory creation

--- src/depression_predictor.py
+++ src/depression_predictor.py
@@ -135,7 +135,7 @@ class StudentDepressionPredictor:
     def ensure_images_dir(self):
 
         if not os.path.exists('images'):
-            os.makedirs('images')
+            os.makedirs('images', exist_ok=True)
             print("Created 'images' directory for saving visualizations")
         return os.path.join(os.getcwd(), 'images')
 
